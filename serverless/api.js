9ec11d6f8fdc31b7b7fc0440ccb61b63456b50bc
Parth <parthparadkar3@gmail.com>
2022-12-06 17:25:48 +0530

    Create web app

--- serverless/api.js
+++ serverless/api.js
@@ -8,7 +8,29 @@ const bodyParser = require('body-parser');
 const tableName = process.env.EMAILS_TABLE;
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views');
+
 app.use(bodyParser.json({ strict: false }));
+app.use(express.static('public'));
+
+app.get('/bookmark/:id', (req, res) => {
+    const params = {
+        TableName: tableName,
+        Key: {
+            s3Key: req.params.id,
+        }
+    }
+    dynamoDb.get(params, (err, data) => {
+        if(err) {
+            console.log(err)
+            res.status(400).json({ error: 'Could not get bookmark' });   
+        }
+        else {
+            res.render('bookmark', { subject: data.Item.subject, htmlContent: data.Item.htmlContent })
+        }
+    })
+})
 
 app.get('/bookmarks', (req, res) => {
     const params = {
@@ -20,7 +42,7 @@ app.get('/bookmarks', (req, res) => {
             res.status(400).json({ error: 'Could not get bookmarks' });
         }
         if(result.Items) {
-            res.json(result.Items)
+            res.render('index', { emails: result.Items.map((value) => { return { url: `/dev/bookmark/${value.s3Key}`, subject: value.subject } }) })
         } else {
             res.status(400).json({ error: 'No bookmarks found!' });
         }
